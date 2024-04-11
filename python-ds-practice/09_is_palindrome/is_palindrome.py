969c72619edd8424452076ab81b710ec03135871
sdelgado0125 <sdelgado0125@gmail.com>
2024-04-11 13:57:18 -0400

    python scan

---
+++ python-ds-practice/09_is_palindrome/is_palindrome.py
@@ -0,0 +1,25 @@
+def is_palindrome(phrase):
+    """Is phrase a palindrome?
+
+    Return True/False if phrase is a palindrome (same read backwards and
+    forwards).
+
+        >>> is_palindrome('tacocat')
+        True
+
+        >>> is_palindrome('noon')
+        True
+
+        >>> is_palindrome('robert')
+        False
+
+    Should ignore capitalization/spaces when deciding:
+
+        >>> is_palindrome('taco cat')
+        True
+
+        >>> is_palindrome('Noon')
+        True
+    """
+    words = phrase.lower().replace(" ", "")
+    return words == words[::-1]
---
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -0,0 +1,21 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    result = ""
+    for ltr in phrase:
+        if ltr.lower() == to_swap.lower():
+            result += ltr.swapcase()
+        else:
+            result+= ltr
+    return result
+    
